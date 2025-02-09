4e64055b35f4287511150ea37feda2f011f83a21
ImLTHQ <2423336405@qq.com>
2025-02-10 04:53:14 +0800

    1

--- 明文源码.js
+++ 明文源码.js
@@ -4,7 +4,8 @@ import { connect } from 'cloudflare:sockets';
 const SUB_PATH = "XiaoYeTech"; // 订阅路径，支持任意大小写字母和数字， [域名/SUB_PATH] 进入订阅页面
 const V2RAY_PATH = 'v2ray';
 const CLASH_PATH = 'clash';
-const SUB_UUID = "550e8400-e29b-41d4-a716-446655440000"; // 订阅验证 UUID，建议修改为自己的UUID
+const DEFAULT_UUID = '550e8400-e29b-41d4-a716-446655440000';
+const SUB_UUID = env.SUB_UUID || DEFAULT_UUID; // 订阅验证 UUID，建议修改为自己的UUID
 
 let PREFERRED_NODES = [
     //'www.wto.org',
