534f0071f485b3eb8341a523caf0d3ebdc0402b4
ImLTHQ <2423336405@qq.com>
2025-02-11 20:20:49 +0800

    1

--- 明文源码.js
+++ 明文源码.js
@@ -5,7 +5,7 @@ const V2RAY_PATH = 'v2ray';
 const CLASH_PATH = 'clash';
 
 let PREFERRED_NODES_TXT_URL = [
-    'https://raw.githubusercontent.com/ImLTHQ/edgeTunnel/main/Domain.txt',
+    //'https://raw.githubusercontent.com/ImLTHQ/edgeTunnel/main/Domain.txt',
 ];
     // 优选节点 TXT 文件路径
     // 格式: IP(v6也可以哦)/域名:端口#节点名称  端口不填默认443 节点名称不填则使用统一名称，任何都不填使用自身域名
