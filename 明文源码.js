1c5e47db4fdfa62efa5277170dd05abf4de94a2e
ImLTHQ <2423336405@qq.com>
2025-02-12 15:53:02 +0800

    加https://

--- 明文源码.js
+++ 明文源码.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 const V2RAY_PATH = 'v2ray';
 const CLASH_PATH = 'clash';
 
-let PREFERRED_NODES_TXT_URL = 'raw.githubusercontent.com/ImLTHQ/edge-tunnel/refs/heads/main/Domain.txt'; 
+let PREFERRED_NODES_TXT_URL = 'https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/refs/heads/main/Domain.txt'; 
 // 优选节点 TXT 文件路径，使用 TXT 时，脚本内部填写的节点无效，两者二选一
 // 格式: IP(v6也可以哦)/域名:端口#节点名称  端口不填默认443 节点名称不填则使用统一名称，任何都不填使用自身域名
 
