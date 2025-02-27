5db681ecec5fed989571772c0f61c7571888984d
ImXiaoYe <2423336405@qq.com>
2025-02-27 20:50:02 +0800

    1

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import sys
 
-def csv_to_txt(csv_filename, output_filename):
+def csv_to_txt(csv_filename, output_filename, area_name):
     df = pd.read_csv(csv_filename, encoding='utf-8')
     ips = df.iloc[:, 0]
     download_speeds = df.iloc[:, 5]
 
     with open(output_filename, 'w', encoding='utf-8') as f:
         for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
-            f.write(f"{ip}#香港优选IP {i+1}  ↓ {speed}MB/s\n")
+            f.write(f"{ip}#{area_name}优选IP {i+1}  ↓ {speed}MB/s\n")
 
-csv_filename = './test/HK.csv'
-output_filename = './test/HK.txt'
-csv_to_txt(csv_filename, output_filename)
+csv_HK = 'HK.csv'
+output_HK = 'HK.txt'
+area_name_HK = "香港"
+csv_to_txt(csv_HK, output_HK, area_name_HK)
