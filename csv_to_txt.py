11982fa05cb459a2865f34d6af3d8c72f3e98c4e
ImXiaoYe <2423336405@qq.com>
2025-02-27 21:02:18 +0800

    1

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -10,4 +10,9 @@ def csv_to_txt(csv_filename, output_filename, area_name):
         for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
             f.write(f"{ip}#{area_name}优选IP {i+1}  ↓ {speed}MB/s\n")
 
-csv_to_txt("HK.csv", "HK.txt", "香港")
+csv_to_txt("HKG.csv", "HKG.txt", "中国香港")
+csv_to_txt("KHH.csv", "KHH.txt", "中国台湾高雄")
+csv_to_txt("NRT.csv", "NRT.txt", "日本东京")
+csv_to_txt("LAX.csv", "LAX.txt", "美国洛杉矶")
+csv_to_txt("SEA.csv", "SEA.txt", "美国西雅图")
+csv_to_txt("SJC.csv", "SJC.txt", "美国圣何塞")
