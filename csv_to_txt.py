eb99525790a8352189c44950541beea255e36416
ImXiaoYe <2423336405@qq.com>
2025-02-27 20:41:37 +0800

    1

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -10,6 +10,6 @@ def csv_to_txt(csv_filename, output_filename):
         for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
             f.write(f"{ip}#香港优选IP {i+1}  ↓ {speed}MB/s\n")
 
-csv_filename = 'HK.csv'
-output_filename = 'HK.txt'
+csv_filename = './test/HK.csv'
+output_filename = './test/HK.txt'
 csv_to_txt(csv_filename, output_filename)
