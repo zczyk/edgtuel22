8992f686161e10e1de19748953c80b8d9193ff77
ImXiaoYe <2423336405@qq.com>
2025-02-28 17:46:52 +0800

    优选支持中文域名

--- 明文源码.js
+++ 明文源码.js
@@ -15,15 +15,21 @@ let 我的优选TXT = [
   "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/LAX.txt",
   "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SEA.txt",
   "https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/SJC.txt",
-];
-// 格式: 地址:端口#节点名称  端口不填默认443 节点名称不填则使用默认节点名称，任何都不填使用自身域名
+];  // 格式: 地址:端口#节点名称  端口不填默认443 节点名称不填则使用默认节点名称，任何都不填使用自身域名
 
-let 反代IP = "ts.hpc.tw:443";
-// 格式：地址:端口
+let 反代IP = "ts.hpc.tw:443"; // 格式：地址:端口
 
 let 启用SOCKS5全局反代 = false;
-let 我的SOCKS5账号 = "";
-// 格式：账号:密码@地址:端口
+let 我的SOCKS5账号 = "";  // 格式：账号:密码@地址:端口
+
+// Punycode
+const punycode = {
+  toASCII: function (domain) {
+    return domain.replace(/[\u4e00-\u9fa5]/g, function (char) {
+      return "xn--" + char.charCodeAt(0).toString(16);
+    });
+  },
+};
 
 // 网页入口
 export default {
@@ -69,6 +75,14 @@ export default {
 
         // 去重处理
         我的优选 = [...new Set(我的优选)];
+
+        // 对优选节点进行 Punycode 转码
+        我的优选 = 我的优选.map(item => {
+          const [addressPort, nodeName] = item.split("#");
+          const [address, port] = addressPort.split(":");
+          const encodedAddress = punycode.toASCII(address);
+          return `${encodedAddress}:${port || "443"}${nodeName ? "#" + nodeName : ""}`;
+        });
       }
 
       if (url.pathname === `/${订阅路径}`) {
